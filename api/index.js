0bf229cd6951a1825940c998dbe071cd40423f59
Yogesh-10 <yogiyogesh100800@gmail.com>
2021-07-04 18:12:40 +0530

    server and mongodb created

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+app.listen(5000, () => console.log('Server running on port 5000'))
